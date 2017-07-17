df8ee1781fa668550665f6a7b7a727fbec0c8ba5
handharbeni <mhandharbeni@gmail.com>
2017-07-17 10:52:42 +0700

    Update Function

--- application/modules/admin/controllers/Index.php
+++ application/modules/admin/controllers/Index.php
@@ -16,4 +16,25 @@ class Index extends MX_Controller {
         // $data['view'] = 'main_html/contents';
         $this->load->view('main_html/htmls');
     }
+    function list_menu(){
+
+    }
+    function add_menu(){
+        /*cUrl*/
+    }
+    function update_menu(){
+
+    }
+    function delete_menu(){
+
+    }
+
+    function post_order(){
+        /*1 daftar nang admin*/
+        /*2 diterima oleh admin */
+        /*3 di asign ke kurir*/
+        /*4 diterima kurir*/
+        /*5 diproses kurir*/
+        /*6 selesai by customer / cancel by admin & customer*/
+    }
 }
